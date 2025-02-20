1e24a32eae622db972e5f37e547b89209e13b259
GyuPark <qkrtjfrb8@gmail.com>
2025-02-21 01:03:58 +0900

    class_array

---
+++ repos/array/array/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main() {
+	int Array[5] = { 0, 1, 2, 3, 4 };
+
+	int multiArray[3][3] = {
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}
+	};
+	printf("%d", multiArray[0][1]);
+
+	
+	
+	return 0;
+}
