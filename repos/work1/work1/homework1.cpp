9f2acc6664b8f2092c441a6873624318c235c489
GyuPark <qkrtjfrb8@gmail.com>
2025-02-20 22:46:02 +0900

    homework

---
+++ repos/work1/work1/homework1.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int sum(int a, int b) {
+	return a + b;
+}
+
+int main() {
+	int value = -512;
+	printf("%d 입니다\n", value);
+	printf("%d\n", sum(1, 2));
+	int value2;
+	printf("제곱할 숫자 입력:");
+	scanf_s("%d", &value2);
+	printf("제곱은 %d", value2*value2);
+	return 0;
+}
