a44d0fa2a354f060ad054f08d43baa7554bd53f3
GyuPark <qkrtjfrb8@gmail.com>
2025-02-21 22:21:08 +0900

    2_final

--- WORK/array_work/array_work/main.cpp
+++ WORK/array_work/array_work/main.cpp
@@ -3,10 +3,18 @@
 int main() {
 	int array[20];
 	
-	for (int i = 0; i < 20; i++) {
-		array[i] = 2 * (i + 1);
-		printf("%d\n", array[i]);
+	array[0] = 1;
+	array[1] = 1;
+
+	for (int i = 2; i < 20; i++) {
+		array[i] = array[i - 1] + array[i - 2];
+	}
+
+	for (int k = 0; k < 20; k++) {
+		printf("%d\n", array[k]);
 	}
 
 	return 0;
 }
+
+// 1 1 2 3 5 8 13 21 ...
