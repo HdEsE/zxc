8d3c90ec610534a7ca4c82fc13518cad90d75942
GyuPark <qkrtjfrb8@gmail.com>
2025-02-21 21:52:21 +0900

    1_final

--- WORK/repeat_work/repeat_work/main.cpp
+++ WORK/repeat_work/repeat_work/main.cpp
@@ -1,11 +1,19 @@
 #include <cstdio>
 
 int main() {
-	for (int i = 0; i < 5; i++) {
-		for (int k = 0; k < 5; k++) {
+	int raw;
+	int col;
+	printf("가로(행): ");
+	scanf_s("%d", &raw);
+	printf("세로(열): ");
+	scanf_s("%d", &col);
+
+	for (int i = 0; i < raw; i++) {
+		for (int k = 0; k < col; k++) {
 			printf("*");
 		}
 		printf("\n");
 	}
 	return 0;
 }
+
