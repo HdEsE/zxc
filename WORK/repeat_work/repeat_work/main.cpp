79fc71f4dcb3d3d9d6a748e8d6672a40003eb7f2
GyuPark <qkrtjfrb8@gmail.com>
2025-02-21 22:06:15 +0900

    3_final

--- WORK/repeat_work/repeat_work/main.cpp
+++ WORK/repeat_work/repeat_work/main.cpp
@@ -1,14 +1,17 @@
 #include <cstdio>
 
 int main() {
+
 	int input;
-	printf("입력하세요: ");
+	printf("Enter number: ");
+	fseek(stdin, 0, SEEK_END);
 	scanf_s("%d", &input);
 	for (int i = 0; i < input; i++) {
-		for (int k = 0; k < i + 1; k++) {
+		for (int k = 0; k < input - i; k++) {
 			printf("*");
 		}
 		printf("\n");
 	}
+
 	return 0;
 }
