e0b2a24e6d7f2a979424cb872bb4437e13f2586e
GyuPark <qkrtjfrb8@gmail.com>
2025-02-21 23:08:12 +0900

    homework_final_2

--- WORK/array_work/array_work/main.cpp
+++ WORK/array_work/array_work/main.cpp
@@ -17,6 +17,7 @@ int main() {
 		}
 		printf("\n");
 	}
+	printf("\n");
 
 	for (int i = 0; i < 5; i++) {
 		for (int k = 0; k < 5; k++) {
@@ -24,6 +25,7 @@ int main() {
 		}
 		printf("\n");
 	}
+	printf("\n");
 
 	for (int i = 0; i < 5; i++) {
 		for (int k = 0; k < 5; k++) {
