f1ab58d2487ed255d3b154601654165e01e9f513
GyuPark <qkrtjfrb8@gmail.com>
2025-02-21 22:47:55 +0900

    3_final_2

--- WORK/array_work/array_work/main.cpp
+++ WORK/array_work/array_work/main.cpp
@@ -3,9 +3,11 @@
 int main() {
 	int array[10][10];
 
+	for (int i = 0; i < 100; i++) {
+		array[i / 10][i % 10] = i + 1;
+	}
 	for (int i = 0; i < 10; i++) {
 		for (int k = 0; k < 10; k++) {
-			array[i][k] = 10 * i + (k + 1);
 			printf("%d ", array[i][k]);
 		}
 		printf("\n");
